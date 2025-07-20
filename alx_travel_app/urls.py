5464fe2873200510994bc5c8b9d44ed0692b6bfc
chijongweb <160375788+chijongweb@users.noreply.github.com>
2025-07-20 15:41:36 +0200

    Update urls.py

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -14,18 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path
+from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework import permissions
-
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="ALX Travel App API",
+        title="ALX Travel API",
         default_version='v1',
-        description="API documentation for the ALX Travel App",
+        description="API documentation for the ALX Travel app",
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
@@ -33,7 +31,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('listings/', include('listings.urls')),
-    path('', include('listings.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/', include('listings.urls')),  # or whatever app you use
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
